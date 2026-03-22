2626c295a7a0f04f54c0f2e7abdf1c38228b4c8d
etta-emmanuel <ettaemmanuelminteh@gmail.com>
2026-03-22 20:54:41 +0100

    Fallback login route created

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,13 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/login', function () {return redirect()->route('home');})->name('login');
 
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/sign-in', [AuthController::class, 'login'])->name('sign-in');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 
 Route::name('web')->middleware('auth')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/dashboard', [DashboardController::class, 'show'])->name('dashboard.show');
     Route::resource('users', UserController::class)->only(['index', 'show', 'edit']);
 });
