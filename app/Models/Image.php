fcb55562b682c726755f4ddee2a7307de8889ee4
etta-emmanuel <ettaemmanuelminteh@gmail.com>
2026-03-22 23:17:15 +0100

    Image can render diff types, thumbnail, medium, full.

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Database\Factories\ImageFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
@@ -19,5 +20,37 @@ class Image extends Model
     public function imageable(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    protected function thumbnail(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->transform('thumbnail'),
+        );
+    }
+
+    protected function medium(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->transform('medium'),
+        );
+    }
+
+    protected function full(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->transform('full'),
+        );
+    }
+    public function transform(string $preset): string
+    {
+        $source = $this->getAttributeFromArray('img_url');
+
+        return match ($preset) {
+            'thumbnail' => $source,
+            'medium' => $source,
+            'full' => $source,
+            default => $source,
+        };
     }
 }
